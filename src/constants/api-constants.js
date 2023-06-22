8f899b14613063e2d2948c0ff5db60c4608814f1
SergiuPs <sergiu.pislaru@web.de>
2023-06-22 17:17:49 +0200

    API Constants

---
+++ src/constants/api-constants.js
@@ -0,0 +1,9 @@
+export const RestApiConstants = {
+    HOST: 'http://localhost:8080',
+    API_VERSION_1: '/api/v1',
+    HOST_AND_API_VERSION: 'http://localhost:8080/api/v1',
+
+    LOGIN: '/login',
+    REGISTER: '/register',
+    ROLES: 'roles',
+}

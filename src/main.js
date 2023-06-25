87ae9ea8e81a6cafae0ea1ba47dd6dfdb0839d04
SergiuPs <sergiu.pislaru@web.de>
2023-06-25 17:08:53 +0200

    Added Vue Awesome Pagination library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
+import VueAwesomePaginate from "vue-awesome-paginate";
+import "vue-awesome-paginate/dist/style.css";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -10,15 +13,16 @@ import BaseButton from './components/ui/BaseButton.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
 import BaseDialog from './components/ui/BaseDialog.vue';
 
-
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.use(VueAwesomePaginate);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-spinner', BaseSpinner);
 app.component('base-dialog', BaseDialog);
+
 
 app.mount('#app');

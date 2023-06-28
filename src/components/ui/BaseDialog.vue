9a016859a36bec5cde33db40e24c54c9ec5d8e50
SergiuPs <sergiu.pislaru@web.de>
2023-06-28 11:38:22 +0200

    Added scroll and some css styling to BaseDialog

--- src/components/ui/BaseDialog.vue
+++ src/components/ui/BaseDialog.vue
@@ -1,69 +1,71 @@
 <template>
-    <teleport to="body">
-      <div v-if="show" @click="tryClose" class="backdrop"></div>
-      <transition name="dialog">
-        <dialog open v-if="show">
-          <header>
-            <slot name="header">
-              <h2>{{ title }}</h2>
-            </slot>
-          </header>
-          <section>
-            <slot></slot>
-          </section>
-          <menu v-if="!fixed">
-            <slot name="actions">
-              <base-button @click="tryClose">Close</base-button>
-            </slot>
-          </menu>
+	<teleport to="body">
+		<div v-if="show" @click="tryClose" class="backdrop"></div>
+		<transition name="dialog">
+		<dialog open v-if="show">
+			<header>
+				<slot name="header">
+					<h2>{{ title }}</h2>
+				</slot>
+			</header>
+			<div class="modalBody">
+				<section>
+					<slot></slot>
+				</section>
+			</div>
+			<menu v-if="!fixed">
+				<slot name="actions">
+					<base-button @click="tryClose">Close</base-button>
+				</slot>
+			</menu>
         </dialog>
       </transition>
     </teleport>
-  </template>
+</template>
   
-  <script>
-  export default {
-    props: {
-      show: {
-        type: Boolean,
-        required: true,
-      },
-      title: {
-        type: String,
-        required: false,
-      },
-      fixed: {
-        type: Boolean,
-        required: false,
-        default: false,
-      },
+<script>
+export default {
+	props: {
+		show: {
+			type: Boolean,
+			required: true,
+		},
+		title: {
+			type: String,
+			required: false,
+		},
+		fixed: {
+			type: Boolean,
+			required: false,
+			default: false,
+		},
     },
     emits: ['close'],
     methods: {
-      tryClose() {
-        if (this.fixed) {
-          return;
-        }
-        this.$emit('close');
-      },
-    },
-  };
-  </script>
+		tryClose() {
+			if (this.fixed) {
+				return;
+			}
+			this.$emit('close');
+		},
+	},
+};
+</script>
   
-  <style scoped>
-  .backdrop {
-    position: fixed;
+<style scoped>
+.backdrop {
+	position: fixed;
     top: 0;
     left: 0;
     height: 100vh;
     width: 100%;
     background-color: rgba(0, 0, 0, 0.75);
     z-index: 10;
-  }
+}
   
-  dialog {
+dialog {
     position: fixed;
-    top: 20vh;
+    top: 10vh;
     left: 10%;
     width: 80%;
     z-index: 100;
@@ -74,54 +76,59 @@
     margin: 0;
     overflow: hidden;
     background-color: white;
-  }
+}
+
+.modalBody {
+    overflow-y: auto;
+    max-height: 600px;
+}
   
-  header {
-    background-color: #3a0061;
+header {
+    background-color: #e6f6fe;
     color: white;
     width: 100%;
     padding: 1rem;
-  }
+}
   
-  header h2 {
+header h2 {
     margin: 0;
-  }
+}
   
-  section {
+section {
     padding: 1rem;
-  }
+}
   
-  menu {
+menu {
     padding: 1rem;
     display: flex;
     justify-content: flex-end;
     margin: 0;
-  }
+}
   
-  .dialog-enter-from,
-  .dialog-leave-to {
+.dialog-enter-from,
+.dialog-leave-to {
     opacity: 0;
     transform: scale(0.8);
-  }
+}
   
-  .dialog-enter-active {
+.dialog-enter-active {
     transition: all 0.3s ease-out;
-  }
+}
   
-  .dialog-leave-active {
+.dialog-leave-active {
     transition: all 0.3s ease-in;
-  }
+}
   
-  .dialog-enter-to,
-  .dialog-leave-from {
+.dialog-enter-to,
+.dialog-leave-from {
     opacity: 1;
     transform: scale(1);
-  }
+}
   
-  @media (min-width: 768px) {
+@media (min-width: 768px) {
     dialog {
       left: calc(50% - 20rem);
       width: 40rem;
     }
-  }
-  </style>
+}
+</style>

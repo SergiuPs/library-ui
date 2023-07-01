0b8c9f7a7e912189e5201cf5a9a9694fe9766aae
SergiuPs <sergiu.pislaru@web.de>
2023-07-01 12:10:33 +0200

    Added a message bar to TableControls

--- src/components/ui/TableControls.vue
+++ src/components/ui/TableControls.vue
@@ -1,44 +1,74 @@
 <template>
-  <div class="header-box">
-    <div class="left-header">
-				<items-per-page-dropdown></items-per-page-dropdown>
+	<div>
+		<h2>The Users</h2>
+		<div class="transition-wrapper">
+			<transition>
+				<div v-show="showMessageBar" :class="error ? 'message-error' : 'message-ok'">{{ theMessage }}</div>
+			</transition>
 		</div>
-    <div class="center-header">
-      <search-bar></search-bar>
-    </div>
-    <div class="right-header">
-      <base-button  mode="flat" link :to="buttonLink">{{ buttonText }}</base-button>
-    </div>
-  </div>    
+		<div> 
+			<div class="flex-box">
+				<div class="left-child">
+					<items-per-page-dropdown></items-per-page-dropdown>
+				</div>
+				<div class="center-child">
+					<search-bar></search-bar>
+				</div>
+				<div class="right-child">
+					<base-button  mode="flat" link :to="buttonLink">{{ buttonText }}</base-button>
+				</div>
+			</div> 
+		</div>
+	</div>   
 </template>
 
 <script>
 import ItemsPerPageDropdown from './ItemsPerPageDropdown.vue';
 import SearchBar from './SearchBar.vue';
+
 export default {
-  components: {ItemsPerPageDropdown, SearchBar},
-  props: ['buttonText', 'buttonLink']
+	components: {ItemsPerPageDropdown, SearchBar},
+	props: ['buttonText', 'buttonLink', 'showMessageBar', 'theMessage', 'error']
 }
 </script>
 
 <style scoped>
-.header-box {
-  width: 100%;
-  display: flex;
-  flex-flow: row wrap;
-  padding-top: 50px;   
+.flex-box {
+	width: 100%;
+	display: flex;
+	flex-flow: row wrap;  
 }
-.left-header, .center-header, .right-header {
-  flex: 100px; 
+.left-child, .center-child, .right-child {
+	flex: 100px; 
 }
-.header-box div:nth-of-type(1) {
-  text-align: left;
+.flex-box div:nth-of-type(1) {
+	text-align: left;
 }
-.header-box div:nth-of-type(2) {
-  align-self: center;
-  text-align: center;
+.flex-box div:nth-of-type(2) {
+	align-self: center;
+	text-align: center;
 }
-.header-box div:nth-of-type(3) {
-  text-align: right;
+.flex-box div:nth-of-type(3) {
+	text-align: right;
+}
+.v-leave-active {
+	transition: opacity 2s ease;
+}
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+.message-ok {
+	background-color: #E0FFD2;
+}
+.message-error {
+	background-color: #FFE1E1;
+}
+.transition-wrapper {
+	height: 30px;
+}
+h2 {
+	margin: 0;
+	font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif;
 }
 </style>

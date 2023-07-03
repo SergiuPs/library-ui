940a524c6dc32b05779732309ae8498d9e057928
SergiuPs <sergiu.pislaru@web.de>
2023-07-03 19:33:16 +0200

    Added Font Awesome to application

--- src/main.js
+++ src/main.js
@@ -1,19 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-import VueAwesomePaginate from "vue-awesome-paginate";
-import "vue-awesome-paginate/dist/style.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
 import BaseDialog from './components/ui/BaseDialog.vue';
 import TableControls from './components/ui/TableControls.vue';
 import EmptyTable from './components/ui/EmptyTable.vue';
+
+import VueAwesomePaginate from "vue-awesome-paginate";
+import "vue-awesome-paginate/dist/style.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser, faPen, faAt, faKey } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUser, faPen, faAt, faKey);
 
 const app = createApp(App);
 
@@ -27,6 +34,7 @@ app.component('base-spinner', BaseSpinner);
 app.component('base-dialog', BaseDialog);
 app.component('table-controls', TableControls);
 app.component('empty-table', EmptyTable);
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 
 app.mount('#app');

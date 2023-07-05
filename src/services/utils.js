34966922b2eaf797453114a9aba8af310925fb7a
SergiuPs <sergiu.pislaru@web.de>
2023-07-05 19:22:36 +0200

    Method for extracting roles and permissions from token's authorities string

---
+++ src/services/utils.js
@@ -0,0 +1,17 @@
+export  { getRolesAndPermissions }
+
+function getRolesAndPermissions(authorities) {
+    const authoritiesArray = authorities.split(',');
+    const roles = [];
+    const permissions = [];
+
+    for (const authority of authoritiesArray) {
+        if (authority.startsWith('ROLE')) {
+            roles.push(authority);
+        } else {
+            permissions.push(authority)
+        }
+    }
+
+    return [roles, permissions];
+}

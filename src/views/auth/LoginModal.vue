13c2aa701f41a11fa335345efbcf1493156f22fb
SergiuPs <sergiu.pislaru@web.de>
2023-07-26 19:25:47 +0200

    Splitted login modal into LoginForm and ForgottenPassword

---
+++ src/views/auth/LoginModal.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<component :is="selectedComponent"></component>
+		<a @click.prevent="toggleComponent" href="" style="font-size: 13px;">{{ urlText }}</a>
+	</div>
+</template>
+
+<script>
+import ForgottenPassword from './ForgottenPassword.vue';
+import LoginForm from './LoginForm.vue';
+
+export default {
+	components: { LoginForm, ForgottenPassword },
+	inject: ['changeModalTitle'],
+	data() {
+		return {
+			selectedComponent: 'login-form',
+			urlText: 'Forgotten your password?'
+		}
+	},
+	methods: {
+		toggleComponent() {
+			if (this.selectedComponent === 'login-form') {
+				this.changeModalTitle('Reset your password');
+				this.urlText = 'Back to Login';
+				this.selectedComponent = 'forgotten-password';
+			} else {
+				this.changeModalTitle('Welcome back');
+				this.urlText = 'Forgotten your password?';
+				this.selectedComponent = 'login-form';
+			}
+		}
+	}	
+};
+</script>

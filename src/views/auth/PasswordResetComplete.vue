1581f6ee199b844174b8816d2c334cca64fc2342
SergiuPs <sergiu.pislaru@web.de>
2023-07-27 11:24:58 +0200

    PasswordResetComplete view

---
+++ src/views/auth/PasswordResetComplete.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div style="height: 90vh;">
+			<div class="center-div"> 
+				<h5>You have successfully reseted your password.
+					Now you can <a href="#" @click.prevent="showLoginModal">Login</a> with your credentials.
+				</h5>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	inject: ['showLoginModal'],
+}
+</script>

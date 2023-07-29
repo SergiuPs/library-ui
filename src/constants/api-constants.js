d1c26a6b31cecba299358202d53ce3ea6ff0a122
SergiuPs <sergiu.pislaru@web.de>
2023-07-29 21:27:43 +0200

    Api constants related to MyAccount

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -5,6 +5,16 @@ export const RestApiConstants = {
 
     LOGIN: '/login',
     REGISTER: '/register',
+    CONFIRM_REGISTRATION: '/registration-confirmation',
+    REQUEST_SECURITY_CODE: '/security-code-request',
+    VALIDATE_SECURITY_CODE: '/security-code-validation',
+    PASSWORD_RESET_TOKEN: '/password-reset-token',
+    PASSWORD_RESET: '/password-reset',
+    EDIT_EMAIL: '/email-edit',
+
     ROLES: '/roles',
     USERS: '/users',
+    EDIT_PASSWORD: '/password-edit',
+
+    PRESIGNED_URL: '/presigned-url'
 }

9a8fcc8d21ba6903e886426da8b6d8330fc4f4ff
SergiuPs <sergiu.pislaru@web.de>
2023-07-29 21:23:57 +0200

    Avatar urls and prefixes

--- src/constants/app-constants.js
+++ src/constants/app-constants.js
@@ -1,3 +1,7 @@
 export const ApplicationConstants = {
-    PAGINATION_ITEMS_PER_PAGE: 20
+    PAGINATION_ITEMS_PER_PAGE: 20,
+    LOCAL_AVATARS_PATH: 'assets/avatars',
+    LOCAL_AVATARS_PREFIX: 'local',
+    FILE_STORAGE_AVATARS_PATH: 'https://spring-library-avatars.s3.eu-west-2.amazonaws.com',
+    FILE_STORAGE_AVATARS_PREFIX: 's3'
 }

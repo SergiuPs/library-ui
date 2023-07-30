b13b3e46c16f80e85648d2490247b8b0bf55eb86
SergiuPs <sergiu.pislaru@web.de>
2023-07-30 12:36:56 +0200

    Added birthDate and avatar fields to User model

--- src/model/user/user.js
+++ src/model/user/user.js
@@ -1,21 +1,23 @@
 import BaseEntity from "@/model/base-entity";
 
 export default class User extends BaseEntity {
+	userName;
+	email;
 	firstName;
 	lastName;
-	email;
-	userName;
-	password;
+	birthDate;
+	avatar;
 	addresses = [];
 	roles = [];
  
 	constructor(other) {
 		super(other.id, other.version);
+		this.userName = other.userName;
+		this.email = other.email;
 		this.firstName = other.firstName;
 		this.lastName = other.lastName;
-		this.email = other.email;
-		this.userName = other.userName;
-		this.password = other.password;
+		this.birthDate = other.birthDate;
+		this.avatar = other.avatar;
 		this.addresses = other.addresses;
 		this.roles = other.roles;
 	}

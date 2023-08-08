21cd7982fd3124fb9d8ec0e4cab75237d3f35f4b
SergiuPs <sergiu.pislaru@web.de>
2023-08-08 22:05:48 +0200

    userName to username and check for null birthDates

--- src/model/user/user.js
+++ src/model/user/user.js
@@ -1,7 +1,7 @@
 import BaseEntity from "@/model/base-entity";
 
 export default class User extends BaseEntity {
-	userName;
+	username;
 	email;
 	firstName;
 	lastName;
@@ -12,14 +12,16 @@ export default class User extends BaseEntity {
  
 	constructor(other) {
 		super(other.id, other.version);
-		this.userName = other.userName;
+		this.username = other.username;
 		this.email = other.email;
 		this.firstName = other.firstName;
 		this.lastName = other.lastName;
 
-		const birthDate = new Date(other.birthDate);
-		const birthDateWithoutHours = new Date(birthDate.setHours(0,0,0,0))
-		this.birthDate = birthDateWithoutHours;
+		if (other.birthDate) {
+			const birthDate = new Date(other.birthDate);
+			const birthDateWithoutHours = new Date(birthDate.setHours(0,0,0,0))
+			this.birthDate = birthDateWithoutHours;
+		}
 		
 		this.avatar = other.avatar;
 		this.addresses = other.addresses;

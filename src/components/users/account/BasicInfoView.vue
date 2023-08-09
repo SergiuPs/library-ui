c56d0a7c781b6d1b2a32fec05ee4a32455479758
SergiuPs <sergiu.pislaru@web.de>
2023-08-09 14:23:10 +0200

    The Basic Info section of PersonalDetails/EditUser

---
+++ src/components/users/account/BasicInfoView.vue
@@ -0,0 +1,73 @@
+<template>
+	<div> 
+		<div class="user-info-wrapper"> 
+			<div class="margin-tb-auto"> 
+				<div class="grid-half"> 
+					<div class="user-info-block"> 
+						<font-awesome-icon class="user-info-icon" icon="user"/>
+						<span class="user-info-label">Username:</span>
+						<span>{{ username }}</span>
+					</div>
+					<div class="user-info-block">
+						<font-awesome-icon class="user-info-icon" icon="cake-candles"/>
+						<span class="user-info-label">Birth date:</span>
+						<span class="user-info-text">{{ formattedBirthDate }}</span> 
+					</div>
+				</div>
+				<div class="grid-half">
+					<div class="user-info-block">
+						<font-awesome-icon class="user-info-icon" icon="pen"/>
+						<span class="user-info-label">First name:</span>
+						<span class="user-info-text">{{ firstName }}</span>
+					</div>
+					<div class="user-info-block">
+						<font-awesome-icon class="user-info-icon" icon="pen"/>
+						<span class="user-info-label">Last name:</span>
+						<span class="user-info-text">{{ lastName }}</span>
+					</div>
+				</div>
+			</div>
+			<div class="flex-right"> 
+				<base-button class="margin-tb-auto" mode="long" @click="showEditModal">Edit</base-button>
+			</div>
+		</div>
+		<base-dialog :show="isEditModalVisible" :fixed="true" title="Edit Personal Details" @close="closeEditModal">
+			<basic-info-edit
+			:user-id="userId"
+			:username="username"
+			:first-name="firstName"
+			:last-name="lastName"
+			:birth-date="birthDate"
+			@close="closeEditModal">
+			</basic-info-edit>
+		</base-dialog>
+	</div>
+</template>
+
+
+<script>
+import BasicInfoEdit from '@/components/users/account/BasicInfoEdit.vue';
+
+export default {
+	components: { BasicInfoEdit },
+	props: ['userId', 'username', 'birthDate', 'firstName', 'lastName'],
+	data() {
+		return {
+			isEditModalVisible: false,
+		};
+	},
+	computed: {
+		formattedBirthDate() {
+			return this.birthDate ? this.birthDate.toLocaleDateString('en-us', { year:"numeric", month:"long", day:"numeric"}) : '-'; 
+		}
+	},
+	methods: {
+		showEditModal() {
+			this.isEditModalVisible = true;
+		},
+		closeEditModal() {
+			this.isEditModalVisible = false;
+		}
+	}
+}
+</script>
